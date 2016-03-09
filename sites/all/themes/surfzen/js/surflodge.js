5dfe8efc47120876967098217c99bf735c8313d7
bruria1 <bruria@tikke.co.il>
2016-03-09 09:37:22 +0200

    theme hp

---
+++ sites/all/themes/surfzen/js/surflodge.js
@@ -0,0 +1,63 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.my_custom_behavior = {
+  attach: function(context, settings) {
+
+/********  hamburger menu  **********/
+      $(".menu-button").click(function(){
+        if ($("body").hasClass("display-menu")){
+          $("body").removeClass("display-menu");
+        }
+        else {
+          $("body").addClass("display-menu");
+        }
+      });
+
+/**********  main  **********/
+
+  $height = $(window).height()/2;
+  $("#main").css("margin-top", $height);
+  $(window).resize(function() {
+   	$height = $(window).height()/2;
+	$("#main").css("margin-top", $height);
+  });
+
+/*********   scroll  *******/
+
+$linksheight = $height + $("#header").height() -140;
+  $(window).resize(function() {
+	$linksheight = $height + $("#header").height() - 140;
+  });
+$(document).ready(function(){       
+      $scroll_pos = 0;
+      $(document).scroll(function() { 
+        $scroll_pos = $(this).scrollTop();
+        if($scroll_pos > $linksheight) {
+            $("body").addClass('scroll');
+        }
+        else {
+            $("body").removeClass('scroll');
+        }
+      });
+});
+
+
+  }
+};
+
+
+})(jQuery, Drupal, this, this.document);
--- sites/all/themes/surfzen/templates/page.tpl.php
+++ sites/all/themes/surfzen/templates/page.tpl.php
@@ -7,13 +7,21 @@
  * @see https://drupal.org/node/1728148
  */
 ?>
-
-<div id="page">
-
+    <div class="menu-wrapper">
+      <div class="menu-button">
+        <div class="lines">
+          <span class="line1"></span>
+          <span class="line2"></span>
+          <span class="line3"></span>
+        </div>
+      </div>
+    </div>
   <header class="header" id="header" role="banner">
 
     <?php if ($logo): ?>
+    <div class="logo-wrapper">
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
+    </div>
     <?php endif; ?>
 
     <?php if ($site_name || $site_slogan): ?>
@@ -50,27 +58,7 @@
 
   </header>
 
-  <div id="main">
-
-    <div id="content" class="column" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print render($page['content_bottom']); ?>
-      <?php print $feed_icons; ?>
-    </div>
+<div id="page">
 
     <div id="navigation">
 
@@ -97,6 +85,31 @@
 
       <?php print render($page['navigation']); ?>
 
+    </div>
+
+
+
+
+  <div id="main">
+    <?php print render($page['links']); ?>
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print render($page['content_bottom']); ?>
+      <?php print $feed_icons; ?>
     </div>
 
     <?php
