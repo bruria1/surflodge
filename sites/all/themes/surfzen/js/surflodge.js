a9872f240471aee568fa6a2ac04f540d1075cef8
bruria1 <bruria@tikke.co.il>
2016-03-14 10:05:23 +0200

    theme

--- sites/all/themes/surfzen/js/surflodge.js
+++ sites/all/themes/surfzen/js/surflodge.js
@@ -17,6 +17,19 @@
 Drupal.behaviors.my_custom_behavior = {
   attach: function(context, settings) {
 
+function initialize() {
+  var mapOptions = {
+    center: new google.maps.LatLng(40.680898,-8.684059),
+    zoom: 11,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+
+  };
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
 /********  hamburger menu  **********/
       $(".menu-wrapper").click(function(){
         if ($("body").hasClass("display-menu")){
