f9c9b6f63796251f91f6341d8da9544ca961c7bc
bruria1 <bruria@tikke.co.il>
2016-04-17 15:42:47 +0300

    homepage fixes

--- sites/all/themes/surfzen/js/surflodge.js
+++ sites/all/themes/surfzen/js/surflodge.js
@@ -110,12 +110,12 @@ Drupal.behaviors.my_custom_behavior = {
   $frontmainheight = $(window).height()+30;
  // $mousescroll = $(window).height()-100;
   $(".front #main").css("margin-top", $frontmainheight);
-  $(".front .mouse-scroll").css("margin-top", $mousescroll);
+//  $(".front .mouse-scroll").css("margin-top", $mousescroll);
   $(window).resize(function() {
   //  $mousescroll = $(window).height()-100;
     $frontmainheight = $(window).height()+30;
-  $(".front #main").css("margin-top", $frontmainheight);
-    $(".front .mouse-scroll").css("margin-top", $mousescroll);
+    $(".front #main").css("margin-top", $frontmainheight);
+  //  $(".front .mouse-scroll").css("margin-top", $mousescroll);
   });
 
 $height = $(window).height();
